65ec7b20644bb66c17b35d5b7d244849cb14af4a
limodou <limodou@14287918-b64d-0410-abb6-a92efa0c2026>
2011-09-15 02:11:15 +0000

    add timezone app
    git-svn-id: https://uliweb.googlecode.com/svn/trunk@713 14287918-b64d-0410-abb6-a92efa0c2026

---
+++ uliweb/contrib/timezone/__init__.py
@@ -0,0 +1,8 @@
+from uliweb.core.dispatch import bind
+
+@bind('startup_installed')
+def startup_installed(sender):
+    from uliweb.utils.date import set_timezone, set_local_timezone
+    
+    set_timezone(sender.settings.GLOBAL.TIME_ZONE)
+    set_local_timezone(sender.settings.GLOBAL.LOCAL_TIME_ZONE)
