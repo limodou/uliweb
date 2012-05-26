c7677fa065928864a3bcd42b142614f703ec27c6
limodou <limodou@14287918-b64d-0410-abb6-a92efa0c2026>
2012-05-26 10:08:12 +0000

    fix sae
    git-svn-id: https://uliweb.googlecode.com/svn/trunk@912 14287918-b64d-0410-abb6-a92efa0c2026

--- uliweb/contrib/sae/__init__.py
+++ uliweb/contrib/sae/__init__.py
@@ -4,11 +4,9 @@ import uliweb
 def after_init_settings(sender):
     from uliweb import orm, settings
     
-    #if AUTO_SAE_ORM_PARA==True, then use sae to get mysql parameters
-    if settings.ORM.AUTO_SAE_ORM_PARA:
-        import sae.const as const
-        CONNECTION = settings.ORM.CONNECTION.format(username=const.MYSQL_USER,
-        password=const.MYSQL_PASS, host=const.MYSQL_HOST, port=const.MYSQL_PORT,
-        database=const.MYSQL_DB)
-        
-        settings.ORM.CONNECTION = CONNECTION
+    import sae.const as const
+    CONNECTION = settings.ORM.CONNECTION.format(username=const.MYSQL_USER,
+    password=const.MYSQL_PASS, host=const.MYSQL_HOST, port=const.MYSQL_PORT,
+    database=const.MYSQL_DB)
+    
+    settings.ORM.CONNECTION = CONNECTION
