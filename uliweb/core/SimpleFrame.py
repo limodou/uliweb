adf708884dd123cc3229aec2d07bbae171ece420
limodou <limodou@gmail.com>
2014-02-10 15:35:58 +0800

    Fix local_cache bug

--- uliweb/core/SimpleFrame.py
+++ uliweb/core/SimpleFrame.py
@@ -296,8 +296,10 @@ def get_var(key, default=None):
     return settings.get_var(key, default)
 
 def get_local_cache(key, creator=None):
+    global local
+    
     if not hasattr(local, 'local_cache'):
-        local = local_cache = {}
+        local.local_cache = {}
     value = local.local_cache.get(key)
     if value:
         return value
