ff6f4d22ad384dd6e90a1a27d578cc7162133ca8
limodou <limodou@gmail.com>
2014-04-04 21:29:53 +0800

    Fix mysql partition patch

--- uliweb/orm/patch.py
+++ uliweb/orm/patch.py
@@ -6,18 +6,16 @@ from sqlalchemy.ext.compiler import compiles
 @compiles(CreateTable, "mysql")
 def add_partition_scheme(element, compiler, **kw):
     table = element.element
-    partition_by = table.kwargs.pop("mysql_partition_by", None)
-    partitions = table.kwargs.pop("mysql_partitions", None)
+    partition_by = table.kwargs.get("mysql_partition_by", None)
+    partitions = table.kwargs.get("mysql_partitions", None)
 
     ddl = compiler.visit_create_table(element, **kw)
     ddl = ddl.rstrip()
 
     if partition_by:
         ddl += "\nPARTITION BY %s" % partition_by
-        table.kwargs['mysql_partition_by'] = partition_by
     if partitions:
         ddl += "\nPARTITIONS %s" % partitions
-        table.kwargs['mysql_partitions'] = partitions
 
     return ddl
 
