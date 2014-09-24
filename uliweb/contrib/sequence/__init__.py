a429ce567d311b9229e25acdd9be59ec21154f59
limodou <limodou@gmail.com>
2014-09-24 14:39:59 +0800

    Improve sequence

--- uliweb/contrib/sequence/__init__.py
+++ uliweb/contrib/sequence/__init__.py
@@ -1,44 +1,43 @@
 from uliweb import functions
+from time import sleep
 
-def get_sequence(key, default=1, step=1):
+def get_sequence(key, default=1, step=1, retry_times=None, retry_waittime=None):
     from uliweb.orm import SaveError
+    from uliweb import settings
     
     assert step > 0 and default > 0
 
     Sequence = functions.get_model('sequence')
-    row = Sequence.get(Sequence.c.key==key)
-    if row:
-        row.value = row.value + step
-        i = 0
-        while i<3:
-            try:
+    i = 0
+    waittime = retry_waittime or settings.get_var('SEQUENCE/retry_waittime', 0.05)
+    retry_times = retry_times or settings.get_var('SEQUENCE/retry_times', 3)
+    while 1:
+        try:
+            row = Sequence.get(Sequence.c.key==key)
+            if row:
+                row.value = row.value + step
                 row.save(version=True)
-                break
-            except SaveError:
-                i += 1
-                if i == 3:
-                    raise
-    else:
-        row = Sequence(key=key, value=(default+step-1))
-        row.save()
+            else:
+                row = Sequence(key=key, value=(default+step-1))
+                row.save()
+            break
+        except SaveError:
+            i += 1
+            if i == retry_times:
+                raise
+            else:
+                sleep(waittime)
+
     return row.value
 
 def set_sequence(key, value):
-    from uliweb.orm import SaveError
-    
     assert value > 0
 
     Sequence = functions.get_model('sequence')
     row = Sequence.get(Sequence.c.key==key)
     if row:
         row.value = value
-        i = 0
-        while i<3:
-            try:
-                row.save(version=True)
-                break
-            except SaveError:
-                i += 1
+        row.save(version=True)
     else:
         row = Sequence(key=key, value=value)
         row.save()
--- uliweb/contrib/sequence/models.py
+++ uliweb/contrib/sequence/models.py
@@ -3,7 +3,7 @@
 from uliweb.orm import *
 
 class Sequence(Model):
-    key = Field(str, max_length=80, required=True, index=True)
+    key = Field(str, max_length=80, required=True, unique=True, index=True)
     value = Field(int)
     version = Field(int)
     modified_time = Field(datetime.datetime, auto_now=True, auto_now_add=True)
