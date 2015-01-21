04ea711030382903356c04c1587f0c599cea18a5
zhangclb <zhangclb@lenovo.com>
2015-01-21 14:05:43 +0800

    To BooleanField None data means False, so BooleanField need to override validate()

--- uliweb/form/uliform.py
+++ uliweb/form/uliform.py
@@ -34,19 +34,19 @@ def get_id():
     return __id
 
 class D(dict):
-    def __getattr__(self, key): 
-        try: 
+    def __getattr__(self, key):
+        try:
             return self[key]
-        except KeyError, k: 
+        except KeyError, k:
             return None
-        
-    def __setattr__(self, key, value): 
+
+    def __setattr__(self, key, value):
         self[key] = value
-        
+
     def __delattr__(self, key):
-        try: 
+        try:
             del self[key]
-        except KeyError, k: 
+        except KeyError, k:
             raise AttributeError, k
 
 def check_reserved_word(f):
@@ -63,43 +63,43 @@ class FieldProxy(object):
     def __init__(self, form, field):
         self.form = form
         self.field = field
-        
+
     @property
     def label(self):
         return self.get_label()
-    
+
     def get_label(self, _class=None):
         if self.field.__class__ is BooleanField:
             delimeter = False
         else:
             delimeter = True
         return self.field.get_label(_class=_class, delimeter=delimeter)
-    
+
     @property
     def help_string(self):
         return self.field.get_help_string(_class='description')
-    
+
     @property
     def error(self):
         return self.form.errors.get(self.field.field_name, '')
-    
+
     @property
     def html(self):
 #        default = self.field.to_html(self.field.default)
         default = self.field.default
         return self.field.html(self.form.data.get(self.field.field_name, default), self.form.ok)
-    
+
     def __str__(self):
         return self.html
-    
+
     def _get_data(self):
         return self.form.data.get(self.field.name, self.field.default)
-    
+
     def _set_data(self, value):
         self.form.data[self.field.name] = value
-        
+
     data = property(_get_data, _set_data)
-    
+
 class BaseField(object):
     default_build = Text
     field_css_class = ''
@@ -107,8 +107,8 @@ class BaseField(object):
     default_datatype = None
     creation_counter = 0
 
-    def __init__(self, label='', default=None, required=False, validators=None, 
-        name='', html_attrs=None, help_string='', build=None, datatype=None, 
+    def __init__(self, label='', default=None, required=False, validators=None,
+        name='', html_attrs=None, help_string='', build=None, datatype=None,
         multiple=False, idtype=None, static=False, placeholder='', **kwargs):
         self.label = label
         self._default = default
@@ -144,10 +144,10 @@ class BaseField(object):
     def _get_default(self):
         return self._default
     default = property(_get_default)
-    
+
     def to_python(self, data):
         """
-        Convert a data to python format. 
+        Convert a data to python format.
         """
         if data is None:
             return data
@@ -184,14 +184,14 @@ class BaseField(object):
             else:
                 label = str(Tag('span', REQUIRED_CAPTION, _class='field_required')) + label
         return str(Tag('label', label, _for=self.id, newline=False, **kwargs))
-    
+
     def get_help_string(self, **kwargs):
         if self.help_string:
 #            return str(Tag('label', self.help_string, _for=self.id, **kwargs))
             return str(self.help_string)
         else:
             return ''
-    
+
     @property
     def id(self):
         if self._id:
@@ -204,7 +204,7 @@ class BaseField(object):
             else:
                 id = None
             return id
-        
+
     def parse_data(self, request, all_data):
         if not isinstance(request, (tuple, list)):
             request = [request]
@@ -265,25 +265,25 @@ class BaseField(object):
             if msg:
                 return False, msg
         return True, data
-    
+
     def __property_config__(self, form_class, field_name):
         self.form_class = form_class
         self.field_name = field_name
         if not self.name:
             self.name = field_name
-    
+
     def __get__(self, model_instance, model_class):
         if model_instance is None:
             return self
         else:
             return FieldProxy(model_instance, self)
-    
+
     def __set__(self, model_instance, value):
         raise Exception('Virtual property is read-only')
-        
+
 #    def _attr_name(self):
 #        return '_' + self.name + '_'
-#    
+#
 class StringField(BaseField):
     default_datatype = str
     def __init__(self, label='', default='', required=False, validators=None, name='', html_attrs=None, help_string='', build=None, **kwargs):
@@ -291,7 +291,7 @@ class StringField(BaseField):
 
     def to_python(self, data):
         """
-        Convert a data to python format. 
+        Convert a data to python format.
         """
         if data is None:
             return ''
@@ -300,14 +300,14 @@ class StringField(BaseField):
         else:
             data = str(data)
         return data
-    
+
 class UnicodeField(BaseField):
     def __init__(self, label='', default='', required=False, validators=None, name='', html_attrs=None, help_string='', build=None, **kwargs):
         BaseField.__init__(self, label=label, default=default, required=required, validators=validators, name=name, html_attrs=html_attrs, help_string=help_string, build=build, **kwargs)
 
     def to_python(self, data):
         """
-        Convert a data to python format. 
+        Convert a data to python format.
         """
         if data is None:
             return u''
@@ -315,7 +315,7 @@ class UnicodeField(BaseField):
             return data
         else:
             return unicode(data, DEFAULT_ENCODING)
-  
+
 class PasswordField(StringField):
     default_build = Password
 
@@ -349,7 +349,7 @@ class TextField(StringField):
         self.rows = rows
         self.cols = cols
         self.convert_html = convert_html
-    
+
     def html(self, data='', py=True):
 #        if py:
 #            value = self.to_html(data)
@@ -362,7 +362,7 @@ class TextField(StringField):
 
     def to_python(self, data):
         """
-        Convert a data to python format. 
+        Convert a data to python format.
         """
         if data is None:
             return ''
@@ -372,7 +372,7 @@ class TextField(StringField):
             return unicode(data, DEFAULT_ENCODING)
         else:
             return data.encode(DEFAULT_ENCODING)
-    
+
 class TextLinesField(TextField):
     def __init__(self, label='', default=None, required=False, validators=None, name='', html_attrs=None, help_string='', build=None, datatype=str, rows=4, cols=None, **kwargs):
         TextField.__init__(self, label=label, default=default, required=required, validators=validators, name=name, html_attrs=html_attrs, help_string=help_string, build=build, rows=rows, cols=cols, **kwargs)
@@ -395,7 +395,7 @@ class TextLinesField(TextField):
 class BooleanField(BaseField):
     default_build = Checkbox
     field_css_class = 'checkbox'
-    
+
     def __init__(self, label='', default=False, name='', html_attrs=None, help_string='', build=None, required=False, **kwargs):
         BaseField.__init__(self, label=label, default=default, required=False, validators=None, name=name, html_attrs=html_attrs, help_string=help_string, build=build, **kwargs)
 
@@ -416,6 +416,15 @@ class BooleanField(BaseField):
             return 'on'
         else:
             return ''
+
+    def validate(self, data):
+        '''
+        None data means False, so BooleanField need to override validate()
+        '''
+        if data is None:
+            return True, False
+        else:
+            return super(BooleanField, self).validate(data)
 
 class IntField(BaseField):
     default_build = Number
@@ -455,7 +464,7 @@ class SelectField(BaseField):
             self._default = default or []
         else:
             self._default = default or None
-            
+
 #        if self.choices:
 #            self._default = default or self.choices[0][0]
 #        self.validators.append(IS_IN_SET(lambda :self.get_choices()))
@@ -465,7 +474,7 @@ class SelectField(BaseField):
             return self.choices()
         else:
             return self.choices
-        
+
     def html(self, data='', py=True):
 #        if py:
 #            value = self.to_html(data)
@@ -485,7 +494,7 @@ class SelectField(BaseField):
                         choices.insert(0, (choices[0][0], '', self.empty))
                     else:
                         choices.insert(0, ('', self.empty))
-        
+
         return str(self.build(choices, data, id=self.id, name=self.name, multiple=self.multiple, size=self.size, **self.html_attrs))
 
 class RadioSelectField(SelectField):
@@ -493,19 +502,19 @@ class RadioSelectField(SelectField):
 
 class CheckboxSelectField(SelectField):
     default_build = CheckboxSelect
-    
+
     def __init__(self, label='', default=None, choices=None, required=False, validators=None, name='', html_attrs=None, help_string='', build=None, multiple=None, **kwargs):
         multiple = multiple if multiple is not None else True
         SelectField.__init__(self, label=label, default=default, choices=choices, required=required, validators=validators, name=name, html_attrs=html_attrs, help_string=help_string, build=build, multiple=multiple, **kwargs)
-    
+
 class FileField(BaseField):
     default_build = File
-    
+
     def __init__(self, label='', upload_to=None, upload_to_sub=None, **kwargs):
         BaseField.__init__(self, label=label, **kwargs)
         self.upload_to = upload_to
         self.upload_to_sub = upload_to_sub
-    
+
     def to_python(self, data):
         d = D({})
         d['filename'] = os.path.basename(data.filename)
@@ -514,68 +523,68 @@ class FileField(BaseField):
 #        d['length'] = data.file.tell()
 #        data.file.seek(0, os.SEEK_SET)
         return d
-    
+
     def html(self, data, py=True):
 #        if py:
 #            value = self.to_html(data)
 #        else:
 #            value = data
         return str(self.build(name=self.name, id=self.id, **self.html_attrs))
-    
+
 class ImageField(FileField):
-    
+
     def __init__(self, label='', default='', required=False, validators=None, name='', html_attrs=None, help_string='', build=None, size=None, **kwargs):
         FileField.__init__(self, label=label, default=default, required=required, validators=validators, name=name, html_attrs=html_attrs, help_string=help_string, build=build, **kwargs)
         self.size = size
         self.validators.append(IS_IMAGE(self.size))
-    
+
 class _BaseDatetimeField(StringField):
     time_func = 'to_date'
-    
+
     def __init__(self, label='', default=None, required=False, validators=None, name='', html_attrs=None, help_string='', build=None, format=None, **kwargs):
         BaseField.__init__(self, label=label, default=default, required=required, validators=validators, name=name, html_attrs=html_attrs, help_string=help_string, build=build, **kwargs)
         self.format = format
-    
+
     def _get_default(self):
         if self._default == 'now':
             return getattr(date, self.time_func)(date.now())
         else:
             return self._default
     default = property(_get_default)
-    
+
     def to_python(self, data):
         try:
             return getattr(date, self.time_func)(data, format=self.format)
         except ValueError:
             raise Exception, _("The date is not a valid date format.")
-    
+
     def to_html(self, data):
         if data:
             return date.to_string(data, timezone=False)
         else:
             return ''
-    
+
 class DateField(_BaseDatetimeField):
     field_css_class = 'field_date'
 
 class TimeField(_BaseDatetimeField):
     field_css_class = 'field_time'
     time_func = 'to_time'
-    
+
 class DateTimeField(_BaseDatetimeField):
     field_css_class = 'field_datetime'
     time_func = 'to_datetime'
-    
+
 class FormMetaclass(type):
     def __init__(cls, name, bases, dct):
         cls.fields = {}
         cls.fields_list = []
-        
+
         for base in bases[:1]:
             if hasattr(base, 'fields'):
                 for name, field in base.fields.iteritems():
                     cls.add_field(name, field)
-        
+
         fields_list = [(k, v) for k, v in dct.items() if isinstance(v, BaseField)]
         fields_list, dct.items()
         fields_list.sort(lambda x, y: cmp(x[1].creation_counter, y[1].creation_counter))
@@ -590,7 +599,7 @@ class FormBuild(object):
             if t:
                 buf.append(str(t))
         return '\n'.join(buf)
-    
+
 class Form(object):
 
     __metaclass__ = FormMetaclass
@@ -606,9 +615,9 @@ class Form(object):
     form_class = None
     form_id = None
 
-    def __init__(self, action=None, method=None, buttons=None, 
-            validators=None, html_attrs=None, data=None, errors=None, 
-            idtype='name', title='', vars=None, layout=None, 
+    def __init__(self, action=None, method=None, buttons=None,
+            validators=None, html_attrs=None, data=None, errors=None,
+            idtype='name', title='', vars=None, layout=None,
             id=None, _class='', **kwargs):
         self.form_action = action or self.form_action
         self.form_method = method or self.form_method
@@ -626,29 +635,29 @@ class Form(object):
         self.html_attrs = html_attrs or {}
         if '_class' in self.html_attrs:
             self.html_attrs['class'] = self.html_attrs.pop('_class')
-            
+
         self.idtype = idtype
         self.layout = layout or self.layout
         self.vars = vars
         for name, obj in self.fields_list:
             obj.idtype = self.idtype
-        
+
         if self.form_class:
             self.html_attrs['class'] = self.form_class# + ' ' + DEFAULT_FORM_CLASS
-        
+
         if 'class' not in self.html_attrs:
             self.html_attrs['class'] = ''
-            
+
         if self.form_id:
             self.html_attrs['id'] = self.form_id
-  
+
         self.form_class = self.html_attrs.get('class')
         self.form_id = self.html_attrs.get('id')
-        
+
         self.bind(data or {}, errors or {})
         self.__init_validators()
         self.ok = True
-        
+
     @classmethod
     def add_field(cls, field_name, field, attribute=False):
         if isinstance(field, BaseField):
@@ -658,13 +667,13 @@ class Form(object):
             if attribute:
                 setattr(cls, field_name, field)
             cls.fields_list.append((field_name, field))
-        
+
     def __init_validators(self):
         for k, obj in self.fields.items():
             func = getattr(self, 'validate_%s' % obj.field_name, None)
             if func and callable(func):
                 obj.validators.insert(0, func)
-                
+
         func = getattr(self, 'form_validate', None)
         if func and callable(func):
             self.validators.append(func)
@@ -720,7 +729,7 @@ class Form(object):
 
     def __str__(self):
         return self.html()
-    
+
     @property
     def form_begin(self):
         args = self.html_attrs.copy()
@@ -731,20 +740,20 @@ class Form(object):
                 args['enctype'] = "multipart/form-data"
                 break
         return begin_tag('form', **args)
-    
+
     @property
     def form_end(self):
         return '</form>\n'
-    
+
     def get_buttons(self):
         return self._buttons
-    
+
     def bind(self, data=None, errors=None):
         if data is not None:
             self.data = data
         if errors is not None:
             self.errors = errors
-            
+
     def html(self):
         cls = self.layout_class
         layout = cls(self, self.layout, **self.layout_class_args)
@@ -752,7 +761,7 @@ class Form(object):
         body = layout.html()
         post_html = self.post_html() if hasattr(self, 'post_html') else ''
         return ''.join([str(x) for x in [pre_html,body,post_html]])
-    
+
     @property
     def build(self):
         cls = self.layout_class
