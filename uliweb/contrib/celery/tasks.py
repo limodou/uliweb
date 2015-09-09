7b17e983755825f649d539231afb6bec296fbc1d
limodou <limodou@gmail.com>
2015-09-09 20:58:26 +0800

    change async_task from FUNCTIONS to DECORATORS

--- uliweb/contrib/celery/tasks.py
+++ uliweb/contrib/celery/tasks.py
@@ -1,10 +1,10 @@
 # common celery task
 # It'll be used for any common function as celery task
 
-from uliweb import functions
+from uliweb import decorators
 from uliweb.utils.common import import_attr
 
-@functions.async_task
+@decorators.async_task
 def common_celery_task(func, *args, **kwargs):
     f = import_attr(func)
     return f(*args, **kwargs)
