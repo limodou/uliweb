faf367d81fdc9c2b09286446ef630ac47f849034
Chunlin Zhang <zhangclb@lenovo.com>
2015-12-25 08:01:21 +0800

    add auth_type to uliweb.contrib.auth.models.User

--- uliweb/contrib/auth/models.py
+++ uliweb/contrib/auth/models.py
@@ -17,45 +17,46 @@ class User(Model):
     active = Field(bool, verbose_name=_('Active Status'))
     locked = Field(bool, verbose_name=_('Lock Status'))
     deleted = Field(bool, verbose_name=_('Deleted'))
-    
+    auth_type = Field(int, default=get_var('AUTH/AUTH_TYPE_DEFAULT'), verbose_name=_('Auth type'))
+
     def set_password(self, raw_password):
         self.password = encrypt_password(raw_password)
         # self.save()
-    
+
     def check_password(self, raw_password):
         """
         Returns a boolean of whether the raw_password was correct. Handles
         encryption formats behind the scenes.
         """
         return check_password(raw_password, self.password)
-    
+
     def get_image_url(self):
         if self.image:
             return functions.get_href(self.image)
         else:
             return functions.url_for_static('images/user%dx%d.jpg' % (50, 50))
-        
+
     def get_default_image_url(self, size=50):
         return functions.url_for_static('images/user%dx%d.jpg' % (size, size))
-        
+
     def __unicode__(self):
         return (self.nickname or self.username) + (_('(Deleted)') if self.deleted else '')
-    
+
     class Meta:
         display_field = 'username'
-        
+
     class AddForm:
         fields = ['username', 'nickname', 'email', 'is_superuser']
-        
+
     class EditForm:
         fields = ['email']
-        
+
     class AdminEditForm:
         fields = ['email', 'is_superuser']
-        
+
     class DetailView:
         fields = ['username', 'nickname', 'email', 'is_superuser', 'date_join', 'last_login']
-        
+
     class Table:
         fields = [
             {'name':'username'},
@@ -66,7 +67,7 @@ class User(Model):
             {'name':'last_login'},
             {'name':'deleted'},
         ]
-    
+
 class UserGroup(Model):
     name = Field(str, max_length=128, verbose_name=_('Name'), index=True, nullable=True)
     parent = SelfReference(verbose_name=_('Parent Group'), collection_name='children', nullable=True, default=0)
@@ -77,10 +78,10 @@ class UserGroup(Model):
     number_of_people = Field(int, verbose_name=_('Number of People'))
     order = Field(int, verbose_name=_('Order'), default=9999)
     type = Field(CHAR, max_length=1, verbose_name=_('Group Type'), choices=get_var('AUTH/USER_GROUP_TYPE'))
-    
+
     def __unicode__(self):
         return self.name
-    
+
     @classmethod
     def OnInit(cls):
         Index('usergroup_idx', cls.c.parent, cls.c.name)
