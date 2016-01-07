0b16f5194c85787893c75f411eadb2aef63b5e9f
limodou <limodou@gmail.com>
2016-01-07 09:38:36 +0800

    Fix makemodule setup.py template

--- uliweb/template_files/project/setup.py
+++ uliweb/template_files/project/setup.py
@@ -1,9 +1,55 @@
+#coding=utf8
+__doc__ = """{{=project_name}}"""
+
 import re
 import os
 
-from setuptools import setup
+from setuptools import setup, find_packages
+from setuptools.command import build_py as b
 
-__doc__ = """{{=project_name}}"""
+def copy_dir(self, package, src, dst):
+    self.mkpath(dst)
+    for r in os.listdir(src):
+        if r in ['.svn', '_svn']:
+            continue
+        fpath = os.path.join(src, r)
+        if os.path.isdir(fpath):
+            copy_dir(self, package + '.' + r, fpath, os.path.join(dst, r))
+        else:
+            ext = os.path.splitext(fpath)[1]
+            if ext in ['.pyc', '.pyo', '.bak', '.tmp']:
+                continue
+            target = os.path.join(dst, r)
+            self.copy_file(fpath, target)
+
+def find_dir(self, package, src):
+    for r in os.listdir(src):
+        if r in ['.svn', '_svn']:
+            continue
+        fpath = os.path.join(src, r)
+        if os.path.isdir(fpath):
+            for f in find_dir(self, package + '.' + r, fpath):
+                yield f
+        else:
+            ext = os.path.splitext(fpath)[1]
+            if ext in ['.pyc', '.pyo', '.bak', '.tmp']:
+                continue
+            yield fpath
+
+def build_package_data(self):
+    for package in self.packages or ():
+        src_dir = self.get_package_dir(package)
+        build_dir = os.path.join(*([self.build_lib] + package.split('.')))
+        copy_dir(self, package, src_dir, build_dir)
+setattr(b.build_py, 'build_package_data', build_package_data)
+
+def get_source_files(self):
+    filenames = []
+    for package in self.packages or ():
+        src_dir = self.get_package_dir(package)
+        filenames.extend(list(find_dir(self, package, src_dir)))
+    return filenames
+setattr(b.build_py, 'get_source_files', get_source_files)
 
 def fpath(name):
     return os.path.join(os.path.dirname(__file__), name)
